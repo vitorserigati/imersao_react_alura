8eae63ebcb9420a8bb15803ee14069c695e3ef35
Vitor Serigati de Oliveira Basso <79308595+vitorserigati@users.noreply.github.com>
2022-11-14 07:14:28 -0300

    finalizada aula 5

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,32 @@ import Favorites from "../src/components/Favorites";
 import Header from "../src/components/Header";
 import Menu from "../src/components/menu";
 import TimeLine from "../src/components/TimeLine";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { videoService } from "../src/services/videoService";
 
 function HomePage() {
   const [search, setSearch] = useState("");
+  const [playlists, setPlaylists] = useState({ ...config.playlists });
+  const service = videoService();
+
+  useEffect(() => {
+    service.getAllVideos().then((data) => {
+      const novaPlaylist = { ...playlists };
+      data.data.forEach((video) => {
+        novaPlaylist[video.playlist]?.includes(video)
+          ? false
+          : novaPlaylist[video.playlist]?.push(video);
+      });
+      setPlaylists(novaPlaylist);
+    });
+  }, []);
+
   return (
     <>
       <div>
         <Menu searchInput={setSearch} searchValue={search} />
         <Header user={config} />
-        <TimeLine searchValue={search} playlists={config.playlists} />
+        <TimeLine searchValue={search} playlists={playlists} />
         <Favorites persons={config.favorites} />
       </div>
     </>
